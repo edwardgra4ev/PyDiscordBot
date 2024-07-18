47fcf8d53ca85f4c453ca7c807b719a33df800c6
Eduard Grachev <gra4ev.ea@gmail.com>
2024-07-18 20:46:58 +0300

    first commit

--- main.py
+++ main.py
@@ -14,12 +14,14 @@ OFFSET = datetime.timedelta(hours=3)
 MEETING_TIME = datetime.time(hour=10, minute=29, second=00, tzinfo=datetime.timezone(offset=OFFSET, name="МСК"))
 SHUTDOWN_TIME = datetime.time(hour=19, minute=00, second=00, tzinfo=datetime.timezone(offset=OFFSET, name="МСК"))
 
+
 @tasks.loop(time=MEETING_TIME)
 async def notification_the_meeting():
     if await not_day_off():
         channel = bot.get_channel(1263053691808780360)
         message = discord.Embed(title="Собрание отдела", description=f"@everyone\nНапоминаю собрание отдела состоится в 10:30.\n[Присоедениться к собранию]({os.getenv('CHANNEL_URL')})", color=0x00ff00)
         await channel.send(embed=message)
+
 
 @tasks.loop(time=SHUTDOWN_TIME)
 async def notification_the_shutdown():
@@ -28,15 +30,18 @@ async def notification_the_shutdown():
         message = discord.Embed(title="Окончание рабочего дня", description=f"@everyone\nВсем спасибо и хорошего вечера", color=0x00ff00)
         await channel.send(embed=message)
 
+
 @bot.command(name="ping")
 async def ping(ctx):
     await ctx.send("pong")
+
 
 async def not_day_off() -> bool:
     weekday = datetime.date.today().weekday()
     if weekday in [5, 6]:
         return False
     return True
+
 
 @bot.event
 async def on_ready():
